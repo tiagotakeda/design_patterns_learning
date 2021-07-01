774f06bd7063edfe21d386c4017c352d6c7a96ff
tiago.takeda <tiago.a.takeda@gmail.com>
2021-07-01 11:58:51 -0300

    singleton

--- singleton/padrao_singleton.py
+++ singleton/padrao_singleton.py
@@ -1,13 +1,13 @@
-class NomeQualquer(object):
+class Singleton(object):
 
     def __new__(cls):       # método que cria um novo objeto, é executado antes de __init__
         if not hasattr(cls, 'instance'):
-            cls.instance = super(NomeQualquer, cls).__new__(cls)
+            cls.instance = super(Singleton, cls).__new__(cls)
             print(f'Criando o objeto {cls.instance}')
         return cls.instance
 
-s1 = NomeQualquer()
+s1 = Singleton()
 print(f'Instância 1: {id(s1)}')
 
-s2 = NomeQualquer()
+s2 = Singleton()
 print(f'Instância 2: {id(s2)}')
